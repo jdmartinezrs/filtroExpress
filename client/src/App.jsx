a79ae9da726c44c95e649741798630f4318560a2
jdmartinezrs <157431644+jdmartinezrs@users.noreply.github.com>
2024-10-08 01:16:38 -0500

    feat: :sparkles: certeing element can be seen now

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,19 +1,22 @@
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import ProtectedRoute from './token/ProtectedRoute';
-
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-
+import { useState } from 'react';
+import Init_register from "./pages/Init_register"
+import Init_login from "./pages/Init_login"
+import Register from './pages/Register'
+import Login from './pages/Login';
+import Start from "./pages/Start"
 
 function App() {
  
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/users" element={<Users />} />
-        <Route path="/login" element={<Login />} />
+      <Route path='/' element={<Start />} />
+      <Route path='/init-register' element={<Init_register />} />
+      <Route path='/init-login' element={<Init_login />} />
+      <Route path='/register' element={<Register />} />
+      <Route path="/login" element={<Login />} />
       </Routes>
     </Router>
   )
---
+++ client/src/pages/Init_login.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import Svglogin from "../components/Svglogin"
+
+const Init_login = () => {
+
+    return (
+        <div className="bg-[url('/fondologin.svg')] bg-cover bg-center h-screen flex justify-center items-center ">
+            <div className="flex flex-col w-[70%] gap-y-8 justify-center items-center">
+                <p className="text-2xl w-[80%] text-center">Inicia sesión y continúa viendo <span className="font-semibold">tus artesanías favoritas</span></p>
+                <div className='flex flex-col gap-y-4 w-[100%]'>
+                    <a href='http://localhost:3000/users/auth/github'><Svglogin svgUrl="/services/github.svg" text="Github" /></a>
+                    <a href='http://localhost:3000/users/auth/discord'><Svglogin svgUrl="/services/discord.svg" text="Discord" /></a>
+                    <a href='http://localhost:3000/users/auth/google'><Svglogin svgUrl="/services/gmail.svg" text="Gmail" /></a>
+                    <a href='/login'><Svglogin svgUrl="/services/user.svg" text="tu cuenta de Ruraq Maki" /></a>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Init_login;
