c70b10339335671a76917a904c92eb7b22506e51
jdmartinezrs <157431644+jdmartinezrs@users.noreply.github.com>
2024-10-07 22:01:04 -0500

    feat: :sparkles: COndiguratin the frontend

---
+++ client/src/token/ProtectedRoute.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Navigate } from 'react-router-dom';
+
+const ProtectedRoute = ({ children }) => {
+    const token = document.cookie.split('; ').find(row => row.startsWith('token='));
+
+    if (!token) {
+        return <Navigate to="/login" />;
+    }
+
+    return children;
+};
+
+export default ProtectedRoute;
---
+++ client/tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: [
+      "./index.html",
+      "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+    theme: {
+      extend: {
+        fontFamily: {
+          bellota: ['Bellota', 'cursive'],
+        },
+        colors : {
+          graySearch: "#3D3D3D",
+          grayUbi: "#D9D9D9",
+        },
+        fontSize: {
+          'xxs': '0.625rem',
+        },
+      },
+    },
+    plugins: [],
+  }
+  
+  
