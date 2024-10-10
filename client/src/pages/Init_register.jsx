2d7ee590676bc8bcaedc9bfc2cb00d2378ca6300
jdmartinezrs <157431644+jdmartinezrs@users.noreply.github.com>
2024-10-09 21:42:55 -0500

    feat: :sparkles: some modifications were done

--- client/src/pages/Init_register.jsx
+++ client/src/pages/Init_register.jsx
@@ -1,32 +1,38 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import Svgregistro from "../components/Svgregistro";
 
 const Init_register = () => {
   const navigate = useNavigate();
+
   const eventoCambiarVista = () => {
-    navigate("/init-login")
+    navigate("/init-login");
   }
 
   return (
-    <div className="bg-[url('/fondologin.svg')] bg-cover bg-center h-screen flex justify-center items-center ">
-        <div className="flex flex-col w-[70%] gap-y-8">
-            {/* <p className="text-2xl">Regístrate ahora y obtén las mejores promociones en <span className="font-semibold">artesanías peruanas</span></p> */}
-            <div className='flex flex-col gap-y-4'>
-                <a href='http://localhost:3000/users/auth/github'><Svgregistro svgUrl="/services/github.svg" text="Github"/></a>
-                <a href='http://localhost:3000/users/auth/discord'><Svgregistro svgUrl="/services/discord.svg" text="Discord"/></a>
-                <a href='http://localhost:3000/users/auth/google'><Svgregistro svgUrl="/services/gmail.svg" text="Gmail"/></a>
-                {/* <a href='/register'><Svgregistro svgUrl="/services/email.svg" text="correo"/></a> */}
-           
-            </div>
-            <div className='flex flex-col justify-center items-center'>
-                {/* <p className='text-2xl'>¿Ya tienes una cuenta?</p> */}
-                {/* <p className='text-2xl underline font-semibold' onClick={eventoCambiarVista}>Inicia sesión</p> */}
-            </div>
+    <div className="bg-[url('/fondologin.svg')] bg-cover bg-center h-screen flex justify-center items-center">
+      <div className="bg-white rounded-lg shadow-lg p-8 w-[90%] max-w-md flex flex-col gap-y-8 opacity-90">
+        <h1 className="text-2xl font-bold text-center text-gray-800">¡Regístrate ahora!</h1>
+       
+        
+        <div className='flex flex-col gap-y-4'>
+          <a href='http://localhost:3000/users/auth/github'>
+            <Svgregistro svgUrl="/services/github.svg" text="Registrarse con Github" />
+          </a>
+          <a href='http://localhost:3000/users/auth/discord'>
+            <Svgregistro svgUrl="/services/discord.svg" text="Registrarse con Discord" />
+          </a>
+          <a href='http://localhost:3000/users/auth/google'>
+            <Svgregistro svgUrl="/services/gmail.svg" text="Registrarse con Gmail" />
+          </a>
         </div>
+        
+        <div className='flex flex-col justify-center items-center'>
+      
+        </div>
+      </div>
     </div>
   );
 };
 
 export default Init_register;
-
